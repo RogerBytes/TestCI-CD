ef1a676df2fb6d646a383047d16e03bd174dbe38
RogerBytes <rogerbytes@vivaldi.net>
2026-04-09 11:05:53 +0200

    ok

---
+++ src/Controller/MailController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MailController extends AbstractController
+{
+    #[Route('/send-mail', name: 'send_mail')]
+    public function sendMail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('demo@example.com')
+            ->to('test@exemple')
+            ->subject('Bonjour depuis Symfo')
+            ->text('Ceci est un email de test')
+            ->html('<h2>Salut</h2>');
+
+        $mailer->send($email);
+
+        return new Response("Mail envoyé");
+    }
+}
